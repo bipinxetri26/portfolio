0cbbc0663840e186ef2bb323aac84786242e5204
bipinxetri26 <bipinxetri26@gmail.com>
2026-02-13 13:30:42 +0545

    Save local changes before pull

--- index.js
+++ index.js
@@ -217,11 +217,6 @@
             this.reset();
         });
 
-        // Download CV
-        // document.getElementById('downloadCV').addEventListener('click', function(e) {
-        //     e.preventDefault();
-        //     alert('Please contact me directly for my CV.');
-        // });
 
         // Smooth scroll
         document.querySelectorAll('a[href^="#"]').forEach(anchor => {
